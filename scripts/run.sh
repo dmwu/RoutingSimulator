79a09866fdc390d6dbc45417d4866135e7d0744c
Dingming Wu <dmwu0506@gmail.com>
2017-11-18 16:07:28 -0600

    change back to random_shuffle

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ../cmake-build-debug
 cmake ..
-make
+make clean;make
 for top in 0 1 2
     do
     for failLink in 0 1024 2048
@@ -10,7 +10,7 @@ for top in 0 1 2
             do
             fileName="top_"$top"_link_"$failLink"_trail_"$trail".txt"
             echo $fileName
-            ./main -o log  -topo "$top" -routing 1 -failLink "$failLink" trafficTraces/fb_5min_1 > "top_"$top"_link_"$failLink"_trail_"$trail".txt"
+            ./main -o log  -topo "$top" -routing 1 -failLink "$failLink" ../trafficTraces/fb_5min_1 > "top_"$top"_link_"$failLink"_trail_"$trail".txt"
         done
     done
 done
