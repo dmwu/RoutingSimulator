899a53a79e21a000d389e4807161c81b3638985d
Dingming Wu <dmwu0506@gmail.com>
2017-11-26 10:01:49 -0600

    don't shuffle

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,9 +6,9 @@ for top in 0 1 2
     do
     for failLink in -1 0 1024 2048
         do
-        for trial in 1 2
+        for trial in 1
             do
-            fileName="top_"$top"_link_"$failLink"_trial_"$trail".txt"
+            fileName="top_"$top"_link_"$failLink"_trial_"$trial".txt"
             echo $fileName
             ./main -o log  -topo "$top" -routing 1 -failLink "$failLink" ../trafficTraces/fb_10min_1 > "top_"$top"_link_"$failLink"_trial_"$trail".txt"
         done
