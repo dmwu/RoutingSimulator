1f9d526fbb9b14cd4653af4605958550d32d57ae
Dingming Wu <dmwu0506@gmail.com>
2018-01-12 21:37:53 -0600

    using smallDebuggingScript as new script

--- scripts/smallDebuggingScript.sh
+++ scripts/smallDebuggingScript.sh
@@ -2,58 +2,34 @@
 cd ../cmake-build-debug
 cmake ..
 make clean;make
-declare -a ratio4trace=("newfb_10min_1")
+declare -a ratio4trace=("perm_K16Ratio4server4096")
 declare -a ratio10trace=("perm_K16Ratio10server10240" "a2rack_K16Ratio10RackSize80")
 
 for trace in "${ratio4trace[@]}"
     do
-    for top in 0 2
+    for top in 0 1 2
         do
-        for linkNum in 1
+        fileName="top_"${top}"_link_reference"${trace}".temp"
+        ./mainSteady -topo ${top} -routing 0 -linkNum 0  -switchNum 0 -failurePos 0 -trafficLevel 0 -trial 1 ../trafficTraces/${trace} > ${fileName} &
+        for linkNum in 1 3 5 10 20
             do
             for pos in 0 1 2
                 do
                 for trial in 1
                     do
                     fileName="top_"${top}"_linkNum_"${linkNum}"pos"${pos}"_trial_"${trial}"_"${trace}".temp"
-                    ./mainSteady -topo ${top} -routing 0 -linkNum ${linkNum}  -switchNum 0 -failurePos ${pos} -trafficLevel 1 ../trafficTraces/${trace} > ${fileName} &
+                    ./mainSteady -topo ${top} -routing 0 -linkNum ${linkNum}  -switchNum 0 -failurePos ${pos} -trafficLevel 0 -trial ${trial} ../trafficTraces/${trace} > ${fileName} &
                 done
             done
         done
+        wait
     done
-    wait
-
-    linkFilename="final_links_"${trace}".res"
+    now=$(date +"%m_%d_%Y")
+    linkFilename="final_links_"${trace}${now}".impact"
     for entry in ./*"link"*${trace}".temp"
         do
         cat ${entry} >> ${linkFilename}
         echo "" >> ${linkFilename}
     done
     rm *.temp
-
-#    for top in 0 2
-#        do
-#        for switchNum in 1 2 3 5 10
-#            do
-#            for pos in 0 1 2
-#                do
-#                for trial in 1
-#                    do
-#                    fileName="top_"${top}"_switchNum_"${switchNum}"pos"${pos}"_trial_"${trial}"_"${trace}".temp"
-#                    ./mainSteady -topo ${top} -routing 0 -linkNum 0 -switchNum ${switchNum} -failurePos ${pos} -trafficLevel 1 ../trafficTraces/${trace} > ${fileName} &
-#                done
-#            done
-#        done
-#    done
-#    wait
-#
-#    nodeFilename="final_nodes_"${trace}".res"
-#    for entry in ./*"switch"*${trace}".temp"
-#        do
-#        cat ${entry} >> ${nodeFilename}
-#        echo "" >> ${nodeFilename}
-#    done
-#    rm *.temp
 done
-
-
